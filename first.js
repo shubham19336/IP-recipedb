6a8adbeb869c2721f9eb40d92c91cde17370d0ea
shubham19336 <shubham19336@iiitd.ac.in>
2022-02-02 16:44:59 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,20 @@
+function name_printer1()
+{
+    document.getElementById("query_handle").innerHTML = "hello1";
+}
+function name_printer2()
+{
+    document.getElementById("query_handle").innerHTML = "hello2";
+}
+function name_printer3()
+{
+    document.getElementById("query_handle").innerHTML = "hello3";
+}
+function name_printer4()
+{
+    document.getElementById("query_handle").innerHTML = "hello4";
+}
+function name_printer5()
+{
+    document.getElementById("query_handle").innerHTML = "hello5";
+}
