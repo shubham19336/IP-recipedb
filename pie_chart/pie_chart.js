f39c59246918c1d2e697616b41688c0f6968711c
shubham19336 <shubham19336@iiitd.ac.in>
2022-02-12 17:27:36 +0530

    added more pie charts

--- pie_chart/pie_chart.js
+++ pie_chart/pie_chart.js
@@ -1,10 +1,5 @@
-
-anychart.onDocumentReady(function () {
-  
-var chart = am4core.create("chartdiv1", am4charts.PieChart);
-
-// Add data
-chart.data = [{
+// try adding data to json files and fetching it here.
+data1=[{
   "country": "Vegetable",
   "value": 501.9
 }, {
@@ -43,23 +38,80 @@ chart.data = [{
 }
 ];
 
-// Add and configure Series
-var pieSeries = chart.series.push(new am4charts.PieSeries());
-pieSeries.dataFields.value = "value";
-pieSeries.dataFields.category = "country";
-pieSeries.labels.template.disabled = true;
-pieSeries.ticks.template.disabled = true;
+data2=[{
+  "country": "Fruit",
+  "value": 15
+},{
+  "country": "Fruit",
+  "value": 15
+},{
+  "country": "Fruit",
+  "value": 15
+},{
+  "country": "Fruit",
+  "value": 15
+},{
+  "country": "Fruit",
+  "value": 15
+},{
+  "country": "Fruit",
+  "value": 15
+}
+];
 
-chart.legend = new am4charts.Legend();
-chart.legend.position = "right";
-chart.legend.scrollable = true; 
-chart.legend.labels.template.truncate = true;
-am4core.addLicense("ch-custom-attribution");
-chart.innerRadius = am4core.percent(60);
+data3=[{
+  "country": "Vegetable",
+  "value": 501.9
+}, {
+  "country": "Czechia",
+  "value": 301.9
+}, {
+  "country": "Ireland",
+  "value": 201.1
+}, {
+  "country": "Germany",
+  "value": 165.8
+}, {
+  "country": "Australia",
+  "value": 139.9
+}, {
+  "country": "Austria",
+  "value": 128.3
+}
+];
 
-var label = pieSeries.createChild(am4core.Label);
-label.text = "{values.value.sum}";
-label.horizontalCenter = "middle";
-label.verticalCenter = "middle";
-label.fontSize = 40;
+datalist=[data1,data2,data3];
+
+
+anychart.onDocumentReady(function () {
+var div = "chartdiv";
+
+for(var i=1;i<=3;i++){
+  var chart = am4core.create("chartdiv"+i, am4charts.PieChart);
+
+  // Add data
+  chart.data = datalist[i-1];
+
+  // Add and configure Series
+  var pieSeries = chart.series.push(new am4charts.PieSeries());
+  pieSeries.dataFields.value = "value";
+  pieSeries.dataFields.category = "country";
+  pieSeries.labels.template.disabled = true;
+  pieSeries.ticks.template.disabled = true;
+
+  chart.legend = new am4charts.Legend();
+  chart.legend.position = "right";
+  chart.legend.scrollable = true; 
+  chart.legend.labels.template.truncate = true;
+  am4core.addLicense("ch-custom-attribution");
+  chart.innerRadius = am4core.percent(60);
+
+  var label = pieSeries.createChild(am4core.Label);
+  label.text = "{values.value.sum}";
+  label.horizontalCenter = "middle";
+  label.verticalCenter = "middle";
+  label.fontSize = 40;
+}
+  
+
 });
