160e08f7a4b9f9ddd6a9632394c7eb22966da77f
shubham19336 <shubham19336@iiitd.ac.in>
2022-02-12 17:51:58 +0530

    cleaned slider.js

--- slider/slider.js
+++ slider/slider.js
@@ -14,65 +14,23 @@ numberFormat = {
 };
 
 $(function(){
-    var slider1 = document.getElementById('slider-round1');
-    noUiSlider.create(slider1, {
-        start: [10, 280],
-        connect: true,
-        // tooltips:[true,true],
-        tooltips:true,
-        step:1,
-        pips: { mode: "count", values: 3,density:4 },
-        range: {
-            'min': 0,
-            'max': 1200
-        },
-        format: numberFormat
-    });
-
-    var slider2 = document.getElementById('slider-round2');
-    noUiSlider.create(slider2, {
-        start: [20, 80],
-        connect: true,
-        // tooltips:[true,true],
-        tooltips:true,
-        step:1,
-        pips: { mode: "count", values: 3 , density:4},
-        range: {
-            'min': 0,
-            'max': 200
-        },
-        format: numberFormat
-    });
-
-    var slider3 = document.getElementById('slider-round3');
-    noUiSlider.create(slider3, {
-        start: [20, 80],
-        connect: true,
-        // tooltips:[true,true],
-        tooltips:true,
-        step:1,
-        pips: { mode: "count", values: 3 , density:4},
-        range: {
-            'min': 0,
-            'max': 200
-        },
-        format: numberFormat
-    });
-
-    var slider4 = document.getElementById('slider-round4');
-    noUiSlider.create(slider4, {
-        start: [20, 80],
-        connect: true,
-        // tooltips:[true,true],
-        tooltips:true,
-        step:1,
-        pips: { mode: "count", values: 3, density:4 },
-        range: {
-            'min': 0,
-            'max': 200
-        },
-        format: numberFormat
-    });
+    var st='slider-round';
+    for(var i=1;i<=4;i++){
+        var slider1 = document.getElementById(st+i);
+        noUiSlider.create(slider1, {
+            start: i==1?[20,600]:[0, 80],
+            connect: true,
+            // tooltips:[true,true],
+            tooltips:true,
+            step:1,
+            pips: { mode: "count", values: 3,density:4 },
+            range: {
+                'min': 0,
+                'max': i==1?1200:200
+            },
+            format: numberFormat
+        });
+    }   
     
 });
 
