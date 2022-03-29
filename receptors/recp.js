f8dfa037c28dfb6a48c166adb12c4c0ea0c86f38
shubham19336 <shubham19336@iiitd.ac.in>
2022-03-29 17:06:23 +0530

    receptors test done

--- receptors/recp.js
+++ receptors/recp.js
@@ -1,39 +1,55 @@
-$(function(){
-    $("#header").load("/header/header.html"); 
-    $("#footer").load("/footer/footer.html"); 
-  });
-  
-  $(document).ready(function () {
-    var pTabItem = $(".prodNav .ptItem");
-    $(pTabItem).click(function () {
-      // Tab nav active functionality
-      $(pTabItem).removeClass("active");
-      $(this).addClass("active");
-  
-      // Tab container active functionality
-      var tabid = $(this).attr("id");
-      console.log(tabid);
-      disp(tabid+'C');
-    //   if(tabid=='pTab1'){}
-    //   else if(tabid=='pTab2') disp('odor_output');
-    //   $(".prodMain").removeClass("active");
-    //   $("#" + tabid + "C").addClass("active");
-  
-      return false;
-    }
-    
-    );
-  });
+function loadItemDetails(jsonData,tableid) {
+  $(tableid).DataTable( {
+      data: jsonData,
+      responsive: true,
+      columns: [
+          { data: "Uniprot Id" },
+          { data: "Entry name" },
+          { data: "Organism" },
+          { data: "Receptor Type" },
+          { data: "URL",
+            "render": function(data, type, row, meta){
+            if(type === 'display'){
+                data = '<a href="' + row.myid + '">' + data + '</a>';
+            }
+            return data;
+            } 
+          }
+      ]
+  })
+}
 
-  function disp(tableid){
-    console.log(tableid);
-    var items=document.getElementsByClassName("prodMain");
-    for(var i=0;i<items.length;i++){
-    items[i].style.display='none';
-    }
-    var x=document.getElementById(tableid);
-    x.style.display='block';
-  }
+$(function(){
+  $("#header").load("/header/header.html"); 
+  $("#footer").load("/footer/footer.html"); 
+  var json1 = $.getJSON({'url': './taster.json', 'async': false});
+  var json2 = $.getJSON({'url': './odorr.json', 'async': false});  
+  loadItemDetails(JSON.parse(json1.responseText),'#taste_output'); 
+  loadItemDetails(JSON.parse(json2.responseText),'#odor_output'); 
+});
+
+function fix(){
+  $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+}
+
+$(document).ready(function () {
+  var pTabItem = $(".prodNav .ptItem");
+  $(pTabItem).click(function () {
+    // Tab nav active functionality
+    $(pTabItem).removeClass("active");
+    $(this).addClass("active");
+    // Tab container active functionality
+    var tabid = $(this).attr("id");
+    $(".prodMain").removeClass("active");
+    $("#" + tabid + "C").addClass("active");
+
+    // resize the tables
+    fix();
+
+    return false;
+  });
+});
+  
 
   
   
