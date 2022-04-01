b2f13f04240013a65105aae543838a0f5da7d90f
shubham19336 <shubham19336@iiitd.ac.in>
2022-04-01 11:29:29 +0530

    recipe id

--- app.py
+++ app.py
@@ -15,15 +15,19 @@ def result():
       conn = sqlite3.connect('my_data.db')
       result = request.form
       print(result)
-      str = "SELECT Recipe_title,Region,Sub_region,servings,Calories,[Protein (g)],[Total lipid (fat) (g)] FROM recp WHERE Continent = '"
+      str = "SELECT Recipe_id,Recipe_title,Region,Sub_region,servings,Calories,[Protein (g)],[Total lipid (fat) (g)] FROM recp WHERE Continent = '"
       str += result['ccontinent']
       str += "'"
       cursor = conn.execute(str)
       return render_template("result.html",data = cursor)
 
-@app.route('/result/rec_info', methods = ['POST','GET'])
+@app.route('/result/rec_info', methods = ['GET'])
 def result2():
+   id=request.args.get('id')
+   print("ye ri id",id)
    print(result)
-   return render_template("base.html",result = result)
+   return render_template("base.html",result = id)
+
+
 if __name__ == '__main__':
    app.run(debug = False)
